bb92b69611053bf4974ad1b7cfc75b016e79c0b7
Madison Vanderbilt <madison@landlocket.com>
2024-09-10 09:38:24 -0500

    adding in case numerous changes

--- src/Pricing.js
+++ src/Pricing.js
@@ -1,15 +1,14 @@
 import "./App.css"
+import "./index.css"
 import React, {useState} from "react"
-import useWindowSize from "./windowSize"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons'
 
 const pricingFeatures = [{title: "Website Building", description: "Build responsive, professional, and up to date websites within a two-week period. This will help establish your online presence on google and other search engines, driving traffic from social media and word of mouth to further explore your hospitality-related business.", price: "1000"}, {title: "Monthly Website Editing", description: "Routine website edits, updates to new technologies, menu items, hours, and other company related changes. This will ensure that your website is always up to date reflecting the information and changes your customers need to know. This will be done through a monthly check in with one of our web designers, however immediate requests can me made any day of the month for a minimal additional charge. \n *can occur on more than a monthly basis, just a routine check of each website's functionalities occurs each month", price: "200/month"}, {title: "Website Hosting", description: "From managing the domain to ensuring the site’s always online, our website hosting services ensure that customers will always be able to access your website.", price: "30/year"}]
-console.log(useWindowSize, pricingFeatures.length)
 
 const PriceItem = ({buildState}) => {
-    const { width, height }= useWindowSize()
-    const box_width = String(width / (pricingFeatures.length + 1)) + "px"
     return (
-        <div className="createItem" style={{width: box_width}}>
+        <div className="createItem">
             <h2 style={{textAlign: "center"}}>{buildState.title}</h2>
             <p>{buildState.description}</p>
             <h2 style={{textAlign: "center"}}>${buildState.price}</h2>
@@ -25,12 +24,22 @@ const Pricing = () => {
         )
     }
     return (
-        <div style={{display: "inline-flex",margin: "1em", maxWidth: "fit-content",
-            marginLeft: "auto",
-            marginRight: "auto"}}>
-            {items.map((category) => <div style={{margin: "1em"}}>{category}</div>)}
+        <div>
+            <table>
+                <tr>
+                {items.map((category) => <th style={{margin: "1em"}}>{category}</th>)}
+                </tr>
+            </table>
+            <h1 style={{textAlign: "center", width: "fit-content", borderRadius: "50%", backgroundColor: "#498ec7"}}>Book a Consulation <span><FontAwesomeIcon  icon={faArrowAltCircleRight}/></span></h1>
         </div> 
     )
 }
 
 export default Pricing
+
+/*
+<div style={{display: "inline-flex", margin: "auto"}}>
+                {items.map((category) => <div style={{margin: "1em"}}>{category}</div>)}
+                </div>
+                
+                */
